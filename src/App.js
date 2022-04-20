9ed799a61f57439ecd7d2c14eff878c390a3766d
aakashp0209 <103042566+aakashp0209@users.noreply.github.com>
2022-04-20 10:52:44 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios  from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
 
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleApi =() => {
+    console.log({ email, password})
+     axios.post('https://reqres.in/api/login',{
+       email : email,
+       password: password
+     })
+     .then(result=>{
+       console.log(result.data)
+       alert("Success")
+     })
+     .catch(error=> {
+       console.log(error)
+       alert("Error")
+     })
+  }
+ 
+  
+
+  return(
+    <div className="App">
+      Email : <input value={email} onChange={handleEmail} type="text" /> <br/>
+      Password :<input value={password} onChange={handlePassword} type="password" /> <br/>
+      <button onClick={handleApi}>Login </button>
+    </div>
+  )
+}
 export default App;
